d2240462b7de9bbcb6479a607e7a609f0c214566
hachemmy <hachejoven@gmail.com>
2026-09-22 01:30:11 +0300

    scroll

--- src/pages/PageAccueil.jsx
+++ src/pages/PageAccueil.jsx
@@ -22,9 +22,24 @@ function PageAccueil() {
                 <AnimationDeDebut onTermine={() => setMontrerIntro(false)} />
             )}
             {/* Fond HC.png fixe, continu sur toute la page avant le footer */}
-            <div className="pointer-events-none absolute inset-0 z-0">
+            {/* Fond mobile : photo visible au niveau du prénom et de la signature */}
+            <div className="pointer-events-none absolute inset-0 z-0 lg:hidden">
                 <div
-                    className="absolute inset-0 bg-contain bg-[50%_15%] bg-no-repeat bg-scroll opacity-[0.22] lg:bg-cover lg:bg-[50%_20%] lg:bg-fixed"
+                    className="absolute inset-0 opacity-[0.22]"
+                    style={{
+                        backgroundImage: `url(${process.env.PUBLIC_URL}/assets/HC.png), url(${process.env.PUBLIC_URL}/assets/HC.png)`,
+                        backgroundSize: 'contain, contain',
+                        backgroundRepeat: 'no-repeat, no-repeat',
+                        backgroundPosition: '50% 4%, 50% 43%',
+                    }}
+                />
+                <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-black/40 to-black" />
+            </div>
+
+            {/* Fond desktop : parallax fixe */}
+            <div className="pointer-events-none absolute inset-0 z-0 hidden lg:block">
+                <div
+                    className="absolute inset-0 bg-cover bg-[50%_20%] bg-fixed opacity-[0.22]"
                     style={{
                         backgroundImage: `url(${process.env.PUBLIC_URL}/assets/HC.png)`,
                     }}
